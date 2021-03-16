d344cccd969a02a000a774123829d63be0363044
Vyacheslav Ryabinin <vyacheslav.ryabinin@hiq.ru>
2021-03-16 19:44:49 +0300

    WTF, example is broken

--- example/lib/counter.dart
+++ example/lib/counter.dart
@@ -1,20 +1,21 @@
 import 'package:built_redux/built_redux.dart';
 import 'package:built_value/built_value.dart';
+
 part 'counter.g.dart';
 
 ReducerBuilder<Counter, CounterBuilder> reducerBuilder =
     new ReducerBuilder<Counter, CounterBuilder>()
-      ..add(CounterActionsNames.increment, (s, a, b) => b.count++);
+      ..add<dynamic>(CounterActionsNames.increment, (s, a, b) => b.count++);
 
 abstract class CounterActions extends ReduxActions {
-  factory CounterActions() => new _$CounterActions();
+  factory CounterActions() => _$CounterActions();
   CounterActions._();
 
-  ActionDispatcher<Null> get increment;
+  ActionDispatcher<void> get increment;
 }
 
 abstract class Counter implements Built<Counter, CounterBuilder> {
-  factory Counter() => new _$Counter._(count: 0);
+  factory Counter() => _$Counter._(count: 0);
   Counter._();
 
   int get count;
